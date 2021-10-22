dc159bc168583b0559ebd2e54c42df6624a8e079
fudepia <fudepia@outlook.jp>
2021-10-22 16:41:57 +0800

    Fix label

--- wavesim.cc
+++ wavesim.cc
@@ -3,13 +3,14 @@
 #include<sets.hh>
 
 std::string plot(d::compact::coord<double, 2> *ptr, di amnt) {
+    return "Unimplemented!";
 }
 
 int main() {
     constexpr double EPS=1e-4;
-    constexpr d::R sampleRange(-2., 2.);
-    consteval di sampleAmt=std::ceil(sampleRange.span()/EPS);
-    consteval double dx=sampleRange.span()/(sampleAmt-1);
+    const d::R sampleRange(-2., 2.);
+    const di sampleAmt=std::ceil(sampleRange.span()/EPS);
+    const double dx=sampleRange.span()/(sampleAmt-1);
     d::compact::coord<double, 2> pts[sampleAmt];
     for(di i=0; i<sampleAmt; ++i)
         pts[i][0]=sampleRange.von()+dx*(double)i;
