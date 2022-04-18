9f14d4d074bbd5e4818b3874e60b049db8829103
fudepia <fudepia@outlook.jp>
2022-04-18 22:48:02 +0800

    update

--- wavesim.cc
+++ wavesim.cc
@@ -1,18 +1,27 @@
-#include<coord.hh>
+/* Particle Structure Wave Simulation
+ * Framework: Force model
+ */
+
+#include<physobj.hh>
 #include<plot.hh>
 #include<sets.hh>
 
-std::string plot(d::compact::coord<double, 2> *ptr, di amnt) {
+std::string plot(d::dyn::compact::mono<double, 2, 2> *ptr, di amnt) {
     return "Unimplemented!";
 }
 
 int main() {
+    // User-control variables
+    constexpr double forceAmp=1;
     constexpr double EPS=1e-4;
+
+    std::cout << std::setprecision(14);
     const d::R sampleRange(-2., 2.);
     const di sampleAmt=std::ceil(sampleRange.span()/EPS);
     const double dx=sampleRange.span()/(sampleAmt-1);
-    d::compact::coord<double, 2> pts[sampleAmt];
+    d::dyn::compact::mono<double, 2, 2> pts[sampleAmt];
     for(di i=0; i<sampleAmt; ++i)
-        pts[i][0]=sampleRange.von()+dx*(double)i;
+        pts[i][0][0]=sampleRange.von()+dx*(double)i;
+    for(di i=0; i<sampleAmt; ++i) std::cout << pts[i] << std::endl;
     return 0;
 }
